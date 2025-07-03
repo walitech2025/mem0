fd184a971c099f6d4a8ab215702b9f78c976491a
walitech2025 <ysy32020@icloud.com>
2025-07-03 22:47:17 +0800

    feat:update

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -1,6 +1,8 @@
-import importlib.metadata
-
-__version__ = importlib.metadata.version("mem0ai")
+try:
+    import importlib.metadata
+    __version__ = importlib.metadata.version("mem0ai")
+except Exception:
+    __version__ = "0.1.0"
 
 from mem0.client.main import AsyncMemoryClient, MemoryClient  # noqa
 from mem0.memory.main import AsyncMemory, Memory  # noqa
